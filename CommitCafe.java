e146dc7388ad7b3315145f3d1c2ba7005e1925d9
Tanishq Bansod <tanishq.bansod@gmail.com>
2026-04-06 13:41:31 -0400

    Implemented Brew and names

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,17 +1,24 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
-    // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String barista = "Tanishq Bansod";
+    private static String nickname = "The Coffee Master"; // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+            cups++;
+            System.out.println(barista + " brewed a cup of " + drink + ". Total cups: " + cups);
         // STUDENT_TODO_2B: Add a second brew-related improvement
+            if (drink.equals("Espresso")) {
+                System.out.println("That was espressoly made for you");
+            } else if (drink.equals("Latte")) {
+                System.out.println("Better latte than never");
+            }
     }
 
     public static void printSummary() {
-        System.out.println("[SUMMARY] " + barista + " brewed " + cups + " cups today.");
+        System.out.println("[SUMMARY] " + nickname + " brewed " + cups + " cups today.");
     }
 
     public static void main(String[] args) {
