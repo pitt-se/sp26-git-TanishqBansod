a3b6395665eea5d039541a42d11f44159f997b1d
Tanishq Bansod <tanishq.bansod@gmail.com>
2026-04-06 13:55:46 -0400

    Merge

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,18 +1,24 @@
 public class CommitCafe {
 
-    private static String barista = "CoffeeBot"; // STUDENT_TODO_1A: Change name
-    private static String title = "[Lead Barista]"; private static String title = "[Lead Barista]"; private static String title = "[Lead Barista]"; // STUDENT_TODO_1B: Add a nickname or title for the barista
+    private static String barista = "Tanishq Bansod";
+    private static String nickname = "The Coffee Master"; // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
-        cups++;
-        System.out.println("CoffeeBot brewed " + drink + ". Commit your coffee, not your code before it compiles.");
-        System.out.println("[BOT CHECK] drink length = " + drink.length()); System.out.println("[BOT CHECK] drink length = " + drink.length()); System.out.println("[BOT CHECK] drink length = " + drink.length()); // STUDENT_TODO_2B: Add a second brew-related improvement
+        // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+            cups++;
+            System.out.println(barista + " brewed a cup of " + drink + ". Total cups: " + cups);
+        // STUDENT_TODO_2B: Add a second brew-related improvement
+            if (drink.equals("Espresso")) {
+                System.out.println("That was espressoly made for you");
+            } else if (drink.equals("Latte")) {
+                System.out.println("Better latte than never");
+            }
     }
 
     public static void printSummary() {
-        System.out.println("[SUMMARY] " + barista + " brewed " + cups + " cups today.");
+        System.out.println("[SUMMARY] " + nickname + " brewed " + cups + " cups today.");
     }
 
     public static void main(String[] args) {
